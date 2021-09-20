0fd51e0d404dd3779d8cfe7d8001f457ba169c91
ahdkhalid <ahdkhalid@yahoo.com>
2021-09-20 19:08:55 +0530

    tweetform and some js on it.

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from django.utils.http import is_safe_url
+from django.conf import settings
 import random
 
 from .models import Tweet
+from .forms import TweetForm
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwarg):
     return render(request,'pages/home.html',context ={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next')
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwarg):
     '''
